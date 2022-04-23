cbbc61b675074c6ce1f170c08d6801a8d70fb373
Burhan <76588697+burhansaleemi@users.noreply.github.com>
2022-04-24 03:33:46 +0500

    Delete src directory

--- src/actions/deleteToDo.js
+++
@@ -1,8 +0,0 @@
-export function deleteToDo(key){
-    return {
-        type: 'DELETE_TO_DO',
-        payload: {
-            id: key
-        }
-    };
-}
--- src/actions/toDo.js
+++
@@ -1,9 +0,0 @@
-export function addToDo(text) {
-    return {
-        type: 'ADD_TO_DO',
-        payload: {
-            id: Math.random(),
-            name: text
-        }
-    };
-}
